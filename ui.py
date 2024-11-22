164cbfb028bfa23a77275fc26f7ebe23ac60a02b
lingk4231075 <strbryjam1220@gmail.com>
2024-11-22 23:02:09 +0900

    체력 / 골드 나타내는 UI 생성

--- ui.py
+++ ui.py
@@ -4,16 +4,35 @@ import play_mode
 
 
 
-
 class UI:
 
     images = [None] * 5
     def __init__(self):
         self.width_image = [160, 41, 128, 120, 120]
         self.height_image = [41, 41, 16, 8, 8]
+        self.draw_rate = [2, 1.7, 1.7, 1.7, 1.7, 2]
+
         self.image_names = ['Heart_Bar_Panel', 'Circle_Box', 'Value_Bar', 'Value_HP', 'Value_Gold']
-        self.draw_x = 0
-        self.draw_y = -400
+        self.image_index = [0, 3, 4, 2, 2, 1]
+        self.canvas_width = 1400
+        self.canvas_height = 800
+
+        self.draw_offset_x = [25]
+        self.draw_offset_y = [self.canvas_height - 25]
+        self.pos_x = [(0 + self.width_image[self.image_index[0]]  / 2)* self.draw_rate[self.image_index[0]],
+                      (50 + self.width_image[self.image_index[2]] / 2) * self.draw_rate[self.image_index[2]],
+                      (50 + self.width_image[self.image_index[2]] / 2) * self.draw_rate[self.image_index[2]],
+                      (50 + self.width_image[self.image_index[2]] /2)* self.draw_rate[self.image_index[2]],
+                      (50 + self.width_image[self.image_index[2]] / 2) * self.draw_rate[self.image_index[2]],
+                      ( + self.width_image[self.image_index[5]] / 2) * self.draw_rate[self.image_index[5]]]
+
+        self.pos_y = [( - 0- self.height_image[self.image_index[0]]  / 2)* self.draw_rate[0] ,
+                      ( - 12 - self.height_image[self.image_index[2]] / 2) * self.draw_rate[2],
+                      ( - 29 - self.height_image[self.image_index[2]] / 2) * self.draw_rate[2],
+                      ( - 12 - self.height_image[self.image_index[2]] / 2) * self.draw_rate[2],
+                      ( - 29 - self.height_image[self.image_index[2]] / 2) * self.draw_rate[2],
+                      ( - 0 - self.height_image[self.image_index[5]] / 2) * self.draw_rate[5]]
+
         for i in range(0, 5):
             if UI.images[i] == None:
                 name = './UI/' + 'HeartBar' + '/' + self.image_names[i] +  '.png'
@@ -28,9 +47,9 @@ class UI:
         pass
 
     def draw(self):
-        for i in range(0,4):
-            print('what')
-            self.images[i].clip_draw(0, 0, self.width_image[i], self.height_image[i], 50, 700, self.width_image[i], self.height_image[i])
+        for i in range(6):
+            d = self.image_index[i]
+            self.images[d].clip_draw(0, 0, self.width_image[d], self.height_image[d], self.pos_x[i] + self.draw_offset_x[0], self.pos_y[i] + self.draw_offset_y[0], self.width_image[d]  * self.draw_rate[i], self.height_image[d] * self.draw_rate[i])
             # self.images[i].clip_draw(0,
             #                          0,
             #                          self.width_image,
