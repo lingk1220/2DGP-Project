9bdaf7777394f2a51cf54bf8dd9860ef043df102
lingk4231075 <strbryjam1220@gmail.com>
2024-11-23 14:35:30 +0900

    pause mode의 ui가 상호작용을 위한 버튼을 그림

--- ui_menubox.py
+++ ui_menubox.py
@@ -6,22 +6,28 @@ class MenuboxUI:
     images = [None] * 7
     image_background = None
     def __init__(self, offset_x, offset_y):
-        self.width_image = [96, 41, 128, 120, 120, 32, 32]
-        self.height_image = [96, 41, 16, 8, 8, 32, 32]
-        self.draw_rate = [6, 1.7, 1.7, 1.7, 1.7, 3, 2, 2]
+        self.width_image = [96, 64, 128, 120, 120, 32, 32]
+        self.height_image = [96, 16 , 16, 8, 8, 32, 32]
+        self.draw_rate = [6, 5, 5, 5, 1.7, 3, 2, 2]
 
+        self.ui_count = 4
+        self.image_count = 2
+        self.image_names = ['Menu_Box_Frame', 'Title_Box', 'Value_Bar', 'Value_HP', 'Value_Gold', 'sun', 'moon']
 
-        self.image_names = ['Menu_Box_Frame', '', 'Value_Bar', 'Value_HP', 'Value_Gold', 'sun', 'moon']
-        self.image_index = [0, 3, 4, 2, 2, 1, 5, 6]
+        self.text_count = 3
+        self.texts = ['Resume', 'Setting', 'Exit Game']
+        self.text_offset = [(77, 0), (82, 0), (48, 0)]
+        self.text_parent = [1, 2, 3]
+        self.image_index = [0, 1, 1, 1, 2, 1, 5, 6]
         self.canvas_width = 1400
         self.canvas_height = 800
 
         self.draw_offset_x = offset_x
         self.draw_offset_y = offset_y
         self.pos_x = [(0 + self.width_image[self.image_index[0]]  / 2)* self.draw_rate[0],
-                      (50 + self.width_image[self.image_index[2]] / 2) * self.draw_rate[2],
-                      (50 + self.width_image[self.image_index[2]] / 2) * self.draw_rate[2],
-                      (50 + self.width_image[self.image_index[2]] /2)* self.draw_rate[2],
+                      (25 + self.width_image[self.image_index[1]] / 2) * self.draw_rate[2],
+                      (25 + self.width_image[self.image_index[2]] / 2) * self.draw_rate[2],
+                      (25 + self.width_image[self.image_index[3]] /2)* self.draw_rate[2],
                       (50 + self.width_image[self.image_index[2]] / 2) * self.draw_rate[2],
                       (-10 + self.width_image[self.image_index[5]] / 2) * self.draw_rate[5],
                       (-1 + self.width_image[self.image_index[6]] / 2) * self.draw_rate[6],
@@ -29,22 +35,22 @@ class MenuboxUI:
                       ]
 
         self.pos_y = [( - 0- self.height_image[self.image_index[0]]  / 2)* self.draw_rate[0] ,
-                      ( - 12 - self.height_image[self.image_index[2]] / 2) * self.draw_rate[2],
-                      ( - 29 - self.height_image[self.image_index[2]] / 2) * self.draw_rate[2],
-                      ( - 12 - self.height_image[self.image_index[2]] / 2) * self.draw_rate[2],
+                      ( - 18 - self.height_image[self.image_index[1]] / 2) * self.draw_rate[2],
+                      ( - 48 - self.height_image[self.image_index[2]] / 2) * self.draw_rate[2],
+                      ( - 78 - self.height_image[self.image_index[3]] / 2) * self.draw_rate[2],
                       ( - 29 - self.height_image[self.image_index[2]] / 2) * self.draw_rate[2],
                       (  7 - self.height_image[self.image_index[5]] / 2) * self.draw_rate[5],
                       (-4 - self.height_image[self.image_index[6]] / 2) * self.draw_rate[6],
                       (-4 - self.height_image[self.image_index[7]] / 2) * self.draw_rate[7]
                       ]
 
-        self.font = load_font('./Font/ByteBounce.TTF', 64)
+        self.menu_font64 = load_font('./Font/ByteBounce.TTF', 64)
 
 
         if MenuboxUI.image_background == None:
             MenuboxUI.image_background = load_image('./UI/MenuBox/Menu_Background3.png')
 
-        for i in range(0, 1):
+        for i in range(0, self.image_count):
             if MenuboxUI.images[i] == None:
                 name = './UI/' + 'MenuBox' + '/' + self.image_names[i] +  '.png'
                 print(name)
@@ -60,9 +66,13 @@ class MenuboxUI:
     def draw(self):
 
         self.image_background.clip_draw(0, 0, 128, 128, self.canvas_width // 2, self.canvas_height // 2, self.canvas_width, self.canvas_height)
-        for i in range(1):
+        for i in range(self.ui_count):
             if i is not 6:
                 d = self.image_index[i]
                 self.images[d].clip_draw(0, 0, self.width_image[d], self.height_image[d], self.pos_x[i] + self.draw_offset_x, self.pos_y[i] + self.draw_offset_y, self.width_image[d]  * self.draw_rate[i], self.height_image[d] * self.draw_rate[i])
 
-        self.font.draw(700, 400, f'Exit Game', (210, 201, 165))
+        for i in range(self.text_count):
+            ui_index = self.text_parent[i]
+            d = self.image_index[ui_index]
+            self.menu_font64.draw(self.text_offset[i][0] + self.pos_x[ui_index] + self.draw_offset_x - ((self.width_image[d]  * self.draw_rate[ui_index]) // 2), self.text_offset[i][1] + self.pos_y[ui_index] + self.draw_offset_y, self.texts[i], (210, 201, 165))
+
